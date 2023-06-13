cafd135d2cb2610cb84dc1546910d79dd0ebdd6c
Shekhar <38083203+indianspeedster@users.noreply.github.com>
2023-06-12 22:47:48 -0400

    Time: 57 ms (94.20%), Space: 45 MB (17.09%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,32 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let count = init
+    function increment(){
+        return ++init
+        
+    }
+    function decrement(){
+        return --init
+        
+    }
+    function reset(){
+        init = count
+        return init
+    }
+    
+    return {
+        increment: increment,
+        decrement: decrement,
+        reset: reset
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
