195362501ed3385b66bfb9ff8133b4a0b6cb77bd
Shekhar <38083203+indianspeedster@users.noreply.github.com>
2024-01-11 22:03:38 -0500

    Time: 31 ms (96.42%) | Memory: 17.4 MB (9.07%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.py
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.py
@@ -2,13 +2,15 @@ class Solution:
     def halvesAreAlike(self, s: str) -> bool:
         dic = set("a e i o u A E I O U".split())
         t = len(s)//2
-        cnt1, cnt2  = 0, 0
-        for letter in s[:t]:
-            if letter in dic:
+        l1, l2 = s[:t], s[t:]
+        cnt1, cnt2,i = 0,0,0
+        while i < t:
+            if l1[i] in dic:
                 cnt1 += 1
-        for letter in s[t:]:
-            if letter in dic:
+            if l2[i] in dic:
                 cnt2 += 1
+            i += 1
+        
         return cnt1 == cnt2
 
         
