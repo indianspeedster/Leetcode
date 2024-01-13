5ab8cdde711cc38dd0549b198ea392c7dbd356e8
Shekhar <38083203+indianspeedster@users.noreply.github.com>
2024-01-13 09:35:46 -0500

    Time: 89 ms (80.49%) | Memory: 18.2 MB (22.41%) - LeetSync

---
+++ 2487-optimal-partition-of-string/optimal-partition-of-string.py
@@ -0,0 +1,15 @@
+class Solution:
+    def partitionString(self, s: str) -> int:
+        
+        cur = set()
+        ans = 1
+        for sub in s:
+            if sub in cur:
+                cur = set()
+                ans += 1
+                cur.add(sub)
+            else:
+                cur.add(sub)
+        return ans
+
+        
