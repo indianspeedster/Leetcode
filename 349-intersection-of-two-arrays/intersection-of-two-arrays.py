584ad7f451d9d0cd56fa56bd07d4e25eb6aecdc2
Shekhar <38083203+indianspeedster@users.noreply.github.com>
2024-01-18 10:58:06 -0500

    Time: 58 ms (32.91%) | Memory: 17.6 MB (17.63%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.py
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.py
@@ -5,18 +5,18 @@ class Solution:
         ans = []
         last = -1
         for num in nums1:
-            i,j = 0,len(nums2)-1
+            start, end = 0,len(nums2)-1
             if num == last:
                 continue
-            while i <= j:
-                mid = (i+j)//2
+            while start <= end:
+                mid = (start+end)//2
                 if nums2[mid] == num:
                     ans.append(num)
                     break
                 if nums2[mid] > num:
-                    j = mid-1
+                    end = mid-1
                 else:
-                    i = mid+1
+                    start = mid+1
             last = num
         return ans
             
