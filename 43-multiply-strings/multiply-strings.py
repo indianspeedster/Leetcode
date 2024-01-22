bac6b0d9bfd48bfacfe7d230ed390f47c15b9e93
Shekhar <38083203+indianspeedster@users.noreply.github.com>
2024-01-22 14:35:30 -0500

    Time: 176 ms (9.15%) | Memory: 16.8 MB (53.25%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.py
@@ -0,0 +1,18 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        ans = 0
+        for i, num in enumerate(num1[::-1]):
+            num = int(num)
+            ans1 = 0
+            carry = 0
+            for j, mul in enumerate(num2[::-1]):
+                mul = int(mul)
+                number = mul * num + carry 
+                print(number)
+                carry, number = divmod(number, 10)
+                ans1 += number * 10 ** j
+            ans1 += carry * 10 ** (j +1) 
+            ans = ans + ans1 * (10**i)
+        return str(ans)
+
+        
