5760aabfaf0903a5da1fa227393facf164ba438e
Shekhar <38083203+indianspeedster@users.noreply.github.com>
2024-01-25 02:09:57 -0500

    Time: 86 ms (92.83%) | Memory: 57 MB (89.42%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.ts
+++ 1250-longest-common-subsequence/longest-common-subsequence.ts
@@ -1,33 +1,22 @@
 function longestCommonSubsequence(text1: string, text2: string): number {
-    const n1 = text1.length;
-    const n2 = text2.length;
-    const dp = new Map<string, number>(); // Change the key type to string
+    const N = text1.length,
+        M = text2.length;
 
-    const dfs = (x: number, y: number): number => {
-        if (x === n1 || y === n2) {
-            return 0;
-        }
-        const key = `${x},${y}`; // Create a unique string key
-        if (dp.has(key)) {
-            return dp.get(key)!; // Use the key to get the value from the map
-        }
-        
-        if (text1[x] === text2[y]) {
-            dp.set(key, 1 + dfs(x + 1, y + 1));
-            return dp.get(key)!;
-        }
-        
-        const result1 = dfs(x + 1, y);
-        const result2 = dfs(x, y + 1);
+    let dp = new Array(M + 1).fill(0);
 
-        if (result1 > result2) {
-            dp.set(key, result1);
-        } else {
-            dp.set(key, result2);
+    for (let i = 1; i <= N; i += 1) {
+        const currDp = [ ...dp ];
+        for (let j = 1; j <= M; j += 1) {
+            if (text1[i-1] === text2[j-1]) {
+                currDp[j] = 1 + dp[j-1];
+            } else {
+                currDp[j] = Math.max(dp[j], currDp[j-1]);
+            }
         }
-        
-        return dp.get(key)!;
+
+        dp = currDp;
     }
 
-    return dfs(0, 0);
-}
+
+    return dp[M];
+};
