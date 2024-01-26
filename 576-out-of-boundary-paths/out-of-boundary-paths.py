fc2e8098d2f34bb9ba9dd9735d95537521220f5f
Shekhar <38083203+indianspeedster@users.noreply.github.com>
2024-01-25 21:28:16 -0500

    Time: 118 ms (40.46%) | Memory: 22.1 MB (44.51%) - LeetSync

--- 576-out-of-boundary-paths/out-of-boundary-paths.py
+++ 576-out-of-boundary-paths/out-of-boundary-paths.py
@@ -1,15 +1,18 @@
 class Solution:
     def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
-        @cache
+        dp = defaultdict(int)
         def dfs(i,j,step):
+
             if step < 0:
                 return 0
             if step >= 0:
                 if i not in range(m) or j not in range(n):
                     return 1
+            if (i,j,step) in dp:
+                return dp[(i,j,step)]
 
-            return dfs(i+1, j, step -1) + dfs(i-1, j, step -1) + dfs(i, j+1, step -1) +dfs(i, j-1, step -1)
-            
+            dp[(i,j,step)] = dfs(i+1, j, step -1) + dfs(i-1, j, step -1) + dfs(i, j+1, step -1) + dfs(i, j-1, step -1)
+            return dp[(i,j,step)]
             
             
         mod = (10**9) + 7
