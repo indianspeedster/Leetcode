ff233d5ed184662e5fdbfe583ee5b1c5f2f97c9d
Shekhar <38083203+indianspeedster@users.noreply.github.com>
2024-02-15 19:51:40 -0500

    Time: 339 ms (68.75%) | Memory: 33.6 MB (71.25%) - LeetSync

--- 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.py
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.py
@@ -1,12 +1,26 @@
 class Solution:
     def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
-        element_dic = Counter(arr)
-        ind = 0
-        for key, val in sorted(element_dic.items(), key = lambda x : x[1]):
-            if k >= val:
-                k -= val
-                ind += 1
-            else:
-                return len(element_dic) - ind
-        
+        # Dictionary to track the frequencies of elements
+        freq_map = Counter(arr)
+
+        # Min heap to track all the frequencies
+        frequencies = list(freq_map.values())
+        heapq.heapify(frequencies)
+
+        # Tracking the number of elements removed
+        elements_removed = 0
+
+        # Traversing all frequencies
+        while frequencies:
+            # Removing the least frequent element
+            elements_removed += heapq.heappop(frequencies)
+
+            # If the number of elements removed exceeds k, return
+            # the remaining number of unique elements
+            if elements_removed > k:
+                # Add 1 for the remaining element
+                return len(frequencies) + 1
+
+        # We have removed all elements, so no unique integers remain
+        # Return 0 in this case
         return 0
