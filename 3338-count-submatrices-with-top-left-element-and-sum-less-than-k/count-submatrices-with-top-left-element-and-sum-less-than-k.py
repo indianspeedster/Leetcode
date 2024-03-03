0a05e3c28f4c52526dab9e7e28e7f462b0ede44c
Shekhar <38083203+indianspeedster@users.noreply.github.com>
2024-03-03 17:10:22 -0500

    Time: 1641 ms (50.00%) | Memory: 66.1 MB (50.00%) - LeetSync

--- 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.py
+++ 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.py
@@ -2,30 +2,21 @@
 import copy
 class Solution:
     def countSubmatrices(self, grid: List[List[int]], k: int) -> int:
-        m, n = len(grid), len(grid[0])
-        count = 0
-        grid2 = [[0 for _ in range(n)] for l in range(m)]
-        top = copy.deepcopy(grid)
-        bottom =  copy.deepcopy(grid)
-        for i in range(m):
-            for j in range(1,n):
-                top[i][j] += top[i][j-1]
-        for i in range(1,m):
-            for j in range(n):
-                bottom[i][j] += bottom[i-1][j]   
-        for i in range(m):
-            for j in range(n):
-                if i == 0 or j == 0:
-                    matrix_sum = grid[i][j]
-                else:
-                    matrix_sum = grid[i][j] + grid2[i-1][j-1]
-                
-                matrix_sum += top[i][j-1] if j >=1 else 0
-                matrix_sum += bottom[i-1][j] if i >= 1 else 0
-
-                grid2[i][j] = matrix_sum
-                if matrix_sum <= k:
-                    count += 1
-        return count
+        R,C = len(grid), len(grid[0])
+        sm = [[0 for _ in range(C)] for _ in range(R)]
+        ct = 0
+        for i in range(R):
+            for j in range(C):
+                q = grid[i][j]
+                if i > 0:
+                    q += sm[i-1][j]
+                if j > 0:
+                    q += sm[i][j-1]
+                if i >0 and j > 0:
+                    q -= sm[i-1][j-1]
+                sm[i][j] = q
+                if q <= k:
+                    ct += 1
+        return ct
                     
         
