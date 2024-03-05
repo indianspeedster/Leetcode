5c7fdfdf1a4a59659adf4f51a3866e75d0a98de7
Shekhar <38083203+indianspeedster@users.noreply.github.com>
2024-03-05 09:29:55 -0500

    Time: 57 ms (33.47%) | Memory: 16.8 MB (63.60%) - LeetSync

--- 985-bag-of-tokens/bag-of-tokens.py
+++ 985-bag-of-tokens/bag-of-tokens.py
@@ -2,7 +2,7 @@ class Solution:
     def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
         low = 0
         high = len(tokens) - 1
-        score = 0
+        res, score = 0, 0
         tokens.sort()
 
         while low <= high:
@@ -10,12 +10,12 @@ class Solution:
                 score += 1
                 power -= tokens[low]
                 low += 1
-
-            elif low < high and score > 0:
+                res = max(res, score)
+            elif score > 0:
                 score -= 1
                 power += tokens[high]
                 high -= 1
             else:
-                return score
+                return res
 
-        return score       
+        return res       
