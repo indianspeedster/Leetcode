e941586d22c2942f0b5cc81a3a525164e890742f
Shekhar <38083203+indianspeedster@users.noreply.github.com>
2024-04-30 23:07:27 -0400

    Time: 38 ms (32.36%) | Memory: 16.5 MB (93.22%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.py
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.py
@@ -4,8 +4,8 @@ class Solution:
         for i, c in enumerate(word):
             if c == ch:
                 ans = ans + word[:i+1][::-1]
-                if (i + 1) in range(len(word)):
-                    ans = ans + word[i+1:]
+                #if (i + 1) in range(len(word)):
+                ans = ans + word[i+1:]
                 return ans
         return word
         
