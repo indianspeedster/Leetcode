1c4473a08202c9f016f0ea49788259dee0220943
Shekhar <38083203+indianspeedster@users.noreply.github.com>
2024-05-14 11:02:08 -0400

    Time: 692 ms (98.19%) | Memory: 16.5 MB (71.32%) - LeetSync

--- 1331-path-with-maximum-gold/path-with-maximum-gold.py
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.py
@@ -1,22 +1,44 @@
 class Solution:
-    def getMaximumGold(self, grid: List[List[int]]) -> int:
-        row, col = len(grid), len(grid[0])
-        
-        def dfs(i, j, visited):
-            if i < 0 or i >= row or j < 0 or j >= col or grid[i][j] == 0 or (i,j) in visited:
-                return 0
-            visited.add((i, j))
-            up = grid[i][j] + dfs(i-1, j, visited.copy())
-            down = grid[i][j] + dfs(i+1, j, visited.copy())
-            left = grid[i][j] + dfs(i, j-1, visited.copy())
-            right = grid[i][j] + dfs(i, j+1, visited.copy())
-            return max(up, down, left, right)
-        
-        ans = 0
-        for i in range(row):
-            for j in range(col):
+    def __init__(self):
+        self.maxGold = 0
+        self.roww = [1, -1, 0, 0]
+        self.coll = [0, 0, -1, 1]
+
+    def checkIfAllNonZeros(self, grid):
+        count = 0
+        for row in grid:
+            for cell in row:
+                if cell != 0:
+                    count += cell
+                else:
+                    return 0
+        return count
+
+    def backtrack(self, grid, x, y, count):
+        if x < 0 or x >= len(grid) or y < 0 or y >= len(grid[0]):
+            return
+
+        if grid[x][y] != 0:
+            curr = grid[x][y]
+            grid[x][y] = 0
+            count += curr
+            self.maxGold = max(self.maxGold, count)
+
+            for i in range(4):
+                newX = x + self.roww[i]
+                newY = y + self.coll[i]
+                self.backtrack(grid, newX, newY, count)
+
+            grid[x][y] = curr
+
+    def getMaximumGold(self, grid):
+        count = self.checkIfAllNonZeros(grid)
+        if count != 0:
+            return count
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
                 if grid[i][j] != 0:
-                    visited = set()
-                    ans = max(ans, dfs(i, j, visited))
-        return ans
-        
+                    self.backtrack(grid, i, j, 0)
+
+        return self.maxGold
