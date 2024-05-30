a0fa05324317fee6e67da8eede11d131763f1433
Shekhar <38083203+indianspeedster@users.noreply.github.com>
2024-05-30 10:42:47 -0400

    Time: 864 ms (25.29%) | Memory: 16.4 MB (93.68%) - LeetSync

--- 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/count-triplets-that-can-form-two-arrays-of-equal-xor.py
+++ 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/count-triplets-that-can-form-two-arrays-of-equal-xor.py
@@ -1,22 +1,17 @@
-
-
 class Solution:
     def countTriplets(self, arr: List[int]) -> int:
-        n = len(arr)
-        prefix_xor = [0] * (n + 1)
-        count = defaultdict(int)
-        total = defaultdict(int)
-        result = 0
+        countdic = defaultdict(int)
+        count = 0
+        for i in range(len(arr)-1):
+            xori = 0
+            for j in range(i+1, len(arr)):
+                xori ^= arr[j-1]
+                xorj = 0
+                for k in range(j, len(arr)):
+                    xorj ^= arr[k]
+                    if xori == xorj:
+                        count += 1
 
-        for i in range(n):
-            prefix_xor[i + 1] = prefix_xor[i] ^ arr[i]
-
-        for k in range(n):
-            if prefix_xor[k + 1] in count:
-                result += count[prefix_xor[k + 1]] * k - total[prefix_xor[k + 1]]
-            count[prefix_xor[k]] += 1
-            total[prefix_xor[k]] += k
-
-        return result
-
+        
+        return count
         
