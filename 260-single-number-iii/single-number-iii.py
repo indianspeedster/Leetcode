ad7f1935f5c3094c9ffb5b76e3d33d7f4f194400
Shekhar <38083203+indianspeedster@users.noreply.github.com>
2024-05-30 20:44:26 -0400

    Time: 57 ms (63.12%) | Memory: 18.4 MB (88.94%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.py
@@ -0,0 +1,10 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        p=0
+        q=0
+        for i in nums :
+            p^=i
+        for i in nums:
+            if ((p&(-p))&i)==0 :
+                q^=i
+        return [p^q,q]
