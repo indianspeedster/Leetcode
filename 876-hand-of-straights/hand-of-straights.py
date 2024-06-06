cccedc20f85c3ee844ad2926e0606f05bf3bae29
Shekhar <38083203+indianspeedster@users.noreply.github.com>
2024-06-06 00:13:36 -0400

    Time: 163 ms (56.75%) | Memory: 18.3 MB (63.87%) - LeetSync

---
+++ 876-hand-of-straights/hand-of-straights.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize != 0:
+            return False
+        handd = Counter(hand)
+        arr = list(handd.keys())
+        heapq.heapify(arr)
+        while arr:
+            ele = arr[0]
+            for i in range(ele, ele+groupSize):
+                if i not in handd:
+                    return False
+                handd[i] -= 1
+                if handd[i] == 0:
+                    if i !=arr[0]:
+                        return False
+                    heapq.heappop(arr)
+        return True
+
+        
