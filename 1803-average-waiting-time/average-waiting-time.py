8746d06ac28c15b8ef558d294f13a47e5175e881
Shekhar <38083203+indianspeedster@users.noreply.github.com>
2024-07-08 23:09:38 -0400

    Time: 770 ms (23.71%) | Memory: 57.2 MB (45.36%) - LeetSync

---
+++ 1803-average-waiting-time/average-waiting-time.py
@@ -0,0 +1,12 @@
+class Solution:
+    def averageWaitingTime(self, customers: List[List[int]]) -> float:
+        curr = customers[0][0]
+        waitTime = 0
+        for start, end in customers:
+            if start > curr:
+                curr = start
+            curr += end
+            waitTime += curr - start
+        return waitTime / (len(customers))
+
+        
