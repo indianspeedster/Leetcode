2231b1556d7a47a5946b5a44bebc9ed52d9bb5cd
Shekhar <38083203+indianspeedster@users.noreply.github.com>
2024-07-28 19:30:03 -0400

    Time: 258 ms (74.56%) | Memory: 17.7 MB (68.40%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -1,37 +1,35 @@
+from typing import List
+
 class Solution:
-    def findTheCity(self, n: int, edges: list[list[int]], distanceThreshold: int) -> int:
-        # first convert graph to adjacency list representation
-        graph: list[list[int]] = [[] for _ in range(n)]  # i-th nodes will have graph[i] neighbors
-        for node1, node2, distance in edges:
-            graph[node1].append([node2, distance])
-            graph[node2].append([node1, distance])
+    def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
+        dist = [[float('inf')] * n for _ in range(n)]
+        for i in range(n):
+            dist[i][i] = 0
+        
 
-        def get_number_of_neighbors_in_distance(source: int) -> int:
-            queue: list[tuple[int, int]] = [
-                (0, source)
-            ]  # distance to node itself is 0, first store distance so heap is "sorted" b distance
-            visited = set()
+        for u, v, w in edges:
+            dist[u][v] = w
+            dist[v][u] = w
+        
 
-            while queue:
-                distance_to_this_node, cur_node = heappop(queue)
-                if not cur_node in visited:
-                    visited.add(cur_node)
-                    for neighbor, distance in graph[cur_node]:
-                        distance_from_source = distance_to_this_node + distance
-                        if distance_from_source <= distanceThreshold:  # ensure that we're allowed to go to this node
-                            heappush(queue, (distance_from_source, neighbor))
+        for k in range(n):
+            for i in range(n):
+                for j in range(n):
+                    if dist[i][j] > dist[i][k] + dist[k][j]:
+                        dist[i][j] = dist[i][k] + dist[k][j]
+        
 
-            # actually you can return len(visited) and with math there will be nothing wrong but actually we have len(visited) - 1 neighbors since we're not neighbor of ourselves
-            return len(visited) - 1
-
-        minimum_number: int = n
-        res: int = None
-
-        for source in range(n):
-            neighbors: int = get_number_of_neighbors_in_distance(source)
-            # we iterate source from smaller to bigger this ensures that we choose node with greater value if they have equal number of neighbors
-            if neighbors <= minimum_number:
-                minimum_number = neighbors
-                res = source
-
-        return res 
+        minReachableCities = float('inf')
+        bestCity = -1
+        
+        for i in range(n):
+            reachableCities = 0
+            for j in range(n):
+                if dist[i][j] <= distanceThreshold:
+                    reachableCities += 1
+            
+            if reachableCities <= minReachableCities:
+                minReachableCities = reachableCities
+                bestCity = i
+        
+        return bestCity
