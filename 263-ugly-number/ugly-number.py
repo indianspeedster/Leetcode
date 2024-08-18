2a83d5b72c12ee511e107555651e0728d40ff774
Shekhar <38083203+indianspeedster@users.noreply.github.com>
2024-08-18 12:53:17 -0400

    Time: 42 ms (19.05%) | Memory: 16.5 MB (46.14%) - LeetSync

--- 263-ugly-number/ugly-number.py
+++ 263-ugly-number/ugly-number.py
@@ -1,14 +1,14 @@
 class Solution:
     def isUgly(self, n: int) -> bool:
-        if n==0:
+        if n <= 0:
             return False
-        def dfs(n):
-            if n == 1:
-                return True
-            ans = False
-            for i in [2,3,5]:
-                if n%i == 0:
-                    ans = ans or dfs(n//i)
-            return ans
-        return dfs(n)
+        while n % 2 == 0:
+            n //= 2
+        while n % 3 == 0:
+            n //= 3
+        while n % 5 == 0:
+            n //= 5
+        return n == 1
+
+        
         
