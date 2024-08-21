a33833ef73445094233addd0ee2c59461fc06d80
Shekhar <38083203+indianspeedster@users.noreply.github.com>
2024-08-21 15:20:31 -0400

    Time: 510 ms (88.38%) | Memory: 23.6 MB (91.61%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -1,31 +1,4 @@
 class Solution:
-    def sortArray(self, nums: List[int]) -> List[int]:
-
-        def mergeSort(nums):
-            if len(nums) <= 1:
-                return nums
-            n = len(nums)
-            l1 = mergeSort(nums[:n//2])
-            l2 = mergeSort(nums[n//2:])
-            return merge(l1, l2)
-        def merge(l1, l2):
-            ans = []
-            i, j = 0, 0
-            while i < len(l1) and j < len(l2):
-                if l1[i] < l2[j]:
-                    ans.append(l1[i])
-                    i += 1
-                else:
-                    
-                    ans.append(l2[j])
-                    j += 1
-            while i < len(l1):
-                ans.append(l1[i])
-                i += 1
-            while j < len(l2):
-                ans.append(l2[j])
-                j += 1
-            return ans
-        return mergeSort(nums)
-            
-        
+    def sortArray(self, nums: list[int]) -> list[int]:
+        nums.sort()
+        return nums
