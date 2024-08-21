50a3bed49de9485c652c8930038f2866cc056f42
Shekhar <38083203+indianspeedster@users.noreply.github.com>
2024-08-21 12:07:15 -0400

    Time: 342 ms (85.73%) | Memory: 17.8 MB (67.26%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,18 +1,11 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        @cache
-        def dfs(i):
-            if i == len(nums) -1 :
-                return True
-            if i >= len(nums):
-                return False
-            steps = nums[i]
-            final = False
-            for j in range(1, steps+1):
-                final = final or dfs(i+j)
-                if final == True:
-                    break
-            return final
-        return dfs(0)
+        dest = len(nums)-1
+        for i in range(len(nums)-1, -1, -1):
+            if nums[i] >= dest-i:
+                dest = i
+        return dest == 0
+
+        
 
         
