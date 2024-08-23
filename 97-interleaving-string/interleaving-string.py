12458f65036d05cfa4ec3acfc98fb9662b199cc6
Shekhar <38083203+indianspeedster@users.noreply.github.com>
2024-08-23 19:25:58 -0400

    Time: 36 ms (85.38%) | Memory: 17.3 MB (29.25%) - LeetSync

--- 97-interleaving-string/interleaving-string.py
+++ 97-interleaving-string/interleaving-string.py
@@ -3,17 +3,24 @@ class Solution:
         if len(s1) + len(s2) != len(s3):
             return False
         
-        dp = [[False] * (len(s2) + 1) for _ in range(len(s1) + 1)]
-        dp[0][0] = True  
+        memo = {}
         
-        for i in range(len(s1) + 1):
-            for j in range(len(s2) + 1):
-                if i > 0 and s1[i - 1] == s3[i + j - 1]:
-                    dp[i][j] = dp[i][j] or dp[i - 1][j]
-                if j > 0 and s2[j - 1] == s3[i + j - 1]:
-                    dp[i][j] = dp[i][j] or dp[i][j - 1]
+        def dfs(i, j):
+            if i == len(s1) and j == len(s2):
+                return True
+            if (i, j) in memo:
+                return memo[(i, j)]
+            
+            if i < len(s1) and s1[i] == s3[i + j] and dfs(i + 1, j):
+                memo[(i, j)] = True
+                return True
+            if j < len(s2) and s2[j] == s3[i + j] and dfs(i, j + 1):
+                memo[(i, j)] = True
+                return True
+            
+            memo[(i, j)] = False
+            return False
         
-        return dp[len(s1)][len(s2)]
-
+        return dfs(0, 0)
 
         
