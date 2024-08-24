2d92a41316ae84e8a1b4a2963a3177d8be51e098
Shekhar <38083203+indianspeedster@users.noreply.github.com>
2024-08-24 16:36:08 -0400

    Time: 95 ms (26.83%) | Memory: 18.3 MB (50.56%) - LeetSync

---
+++ 207-course-schedule/course-schedule.py
@@ -0,0 +1,25 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adjList = defaultdict(list)
+        for i, j in prerequisites:
+            adjList[i].append(j)
+        self.visited = set()
+        def dfs(node):
+            if node in self.visited :
+                
+                return False
+            if adjList[node] == []:
+                return True
+            self.visited.add(node)
+            for nodes in adjList[node]:
+                if not dfs(nodes):
+                    return False
+            self.visited.remove(node)
+            adjList[node] = []
+            return True
+        ans = True
+        for i in range(numCourses):
+            ans = ans and dfs(i)
+        return ans
+            
+                
