edfbbaab794e239af7440960929fba0e86de7869
Shekhar <38083203+indianspeedster@users.noreply.github.com>
2024-08-24 21:56:17 -0400

    Time: 38 ms (44.25%) | Memory: 17.1 MB (5.02%) - LeetSync

---
+++ 91-decode-ways/decode-ways.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        hashMap = {f"{i}" : chr(i + 64) for i in range(1, 27)}
+        @cache
+        def dfs(i):
+            if i == len(s):
+                return 1
+            if s[i] == "0":
+                return 0
+            left =  dfs(i+1)
+            if i+2 not in range(len(s)+1) or s[i:i+2] not in hashMap:
+                right = 0
+            else:
+                right = dfs(i+2 )
+            return left + right
+        return dfs(0)
+       
+        
