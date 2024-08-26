65359210b11597100c2eb16b24bfa35f04f91ecb
Shekhar <38083203+indianspeedster@users.noreply.github.com>
2024-08-26 14:23:15 -0400

    Time: 2538 ms (13.41%) | Memory: 36.9 MB (56.83%) - LeetSync

--- 2035-count-sub-islands/count-sub-islands.py
+++ 2035-count-sub-islands/count-sub-islands.py
@@ -1,33 +1,31 @@
-
 class Solution:
     def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
-        m, n = len(grid2), len(grid2[0])
-        
-        def dfs(i, j):
-            if not (0 <= i < m and 0 <= j < n):
+        m,n = len(grid2), len(grid2[0])
+        def dfs(i,j):
+            if i not in range(m) or j not in range(n):
                 return
             if grid2[i][j] != 1:
                 return
-            visited.add((i, j))
-            grid2[i][j] = 2  # Mark as visited
-            # Explore all four directions
-            dfs(i + 1, j)
-            dfs(i - 1, j)
-            dfs(i, j + 1)
-            dfs(i, j - 1)
-        
+            visited.add((i,j))
+            grid2[i][j] = 2
+            dfs(i+1, j)
+            dfs(i-1, j)
+            dfs(i, j+1)
+            dfs(i, j-1)
         ans = 0
         for i in range(m):
             for j in range(n):
                 visited = set()
                 if grid2[i][j] == 1:
-                    dfs(i, j)
-                flag = True
-                for x, y in visited:
+                    dfs(i,j)
+                Flag = True
+                for x,y in visited:
                     if grid1[x][y] != 1:
-                        flag = False
+                        Flag = False
                         break
-                if visited and flag:
+                if visited and Flag:
                     ans += 1
         return ans
+                
+        
 
