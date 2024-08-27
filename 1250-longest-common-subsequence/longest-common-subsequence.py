0d77897a3ad98a1b5639fc8c55305aaffc627b4e
Shekhar <38083203+indianspeedster@users.noreply.github.com>
2024-08-27 16:36:34 -0400

    Time: 1108 ms (14.63%) | Memory: 310.6 MB (5.69%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -4,13 +4,14 @@ class Solution:
         def dfs(i,j):
             if i == len(text1) or j == len(text2):
                 return 0
-            pick = 0
+
             if text1[i] == text2[j]:
-                pick = 1 + dfs(i+1, j+1)
-            npick = max(dfs(i+1, j), dfs(i, j+1))
-            return max(pick, npick)
-        return dfs(0,0)
-        
-
-
+                return 1 + dfs(i+1, j+1)
+            else:
+                return max(dfs(i+1, j), dfs(i, j+1))
+        ans = dfs(0,0)
+        if ans < 0:
+            return 0
+        else:
+            return ans 
         
