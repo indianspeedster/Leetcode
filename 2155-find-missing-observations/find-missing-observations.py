30a812e0a6e9b2a4caf52179180f94630bc4ab21
Shekhar <38083203+indianspeedster@users.noreply.github.com>
2024-09-04 23:27:18 -0400

    Time: 1087 ms (14.17%) | Memory: 26.4 MB (80.48%) - LeetSync

---
+++ 2155-find-missing-observations/find-missing-observations.py
@@ -0,0 +1,17 @@
+class Solution:
+    def missingRolls(self, rolls: List[int], mean: int, n: int) -> List[int]:
+        m = len(rolls)
+        total = (m+n) * mean
+        toAchieve = total - sum(rolls)
+        ans = []
+        k = toAchieve / n
+        print(k)
+        if k > 6 or k < 1:
+            return []
+        while n > 0:
+            ans.append(ceil(toAchieve/n))
+            toAchieve -= ceil(toAchieve/n)
+            n -= 1
+        return ans
+
+        
