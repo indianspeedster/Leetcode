0a09d0f1360d52e43db8c1e7bf244c639d8660e7
Shekhar <38083203+indianspeedster@users.noreply.github.com>
2024-09-30 23:54:01 -0400

    Time: 514 ms (40.78%) | Memory: 30.5 MB (57.83%) - LeetSync

--- 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.py
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.py
@@ -2,23 +2,15 @@ class Solution:
     def canArrange(self, arr: List[int], k: int) -> bool:
         hashSet = defaultdict(int)
         for i in arr:
-         
             modval = i % k
-    
             if modval == 0:
                 if hashSet[0] > 0:
                     hashSet[0] -= 1
                 else:
                     hashSet[0] += 1
-                if hashSet[0] == 0:
-                    del hashSet[0]
             elif hashSet[k-modval] > 0:
                 hashSet[k-modval] -= 1
-                if hashSet[k-modval] == 0:
-                    del hashSet[k-modval]
             else:
                 hashSet[modval] += 1
-            
-        
         return sum(hashSet.values()) == 0
         
