7cac201936b89d373ebc49a8a6130e032f6b5f8b
Shekhar <38083203+indianspeedster@users.noreply.github.com>
2024-10-14 13:42:23 -0400

    Time: 166 ms (38.20%) | Memory: 17.3 MB (83.66%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -2,27 +2,26 @@ class Solution:
     def minWindow(self, s: str, t: str) -> str:
         ans = ""
         minLen = len(s)
-        left = 0
-        def match(c1, c2):
-            for key in c2.keys():
-                if key not in c1:
-                    return False
-                if c1[key] < c2[key]:
-                    return False
-            return True
         tDict = Counter(t)
+        need = len(tDict.keys())
+        have = 0
+        left = 0
         sDict = {}
         for i, char in enumerate(s):
             if char not in sDict:
                 sDict[char] = 0
             sDict[char] += 1
-            while match(sDict, tDict):
+            if sDict[char] == tDict[char]:
+                have += 1
+            while have == need:
                 curLen = i-left+1
                 if curLen <= minLen:
-                    minLen = min(minLen, curLen)
+                    minLen = curLen
                     ans = s[left:i+1]
                 sDict[s[left]] -= 1
+                if s[left] in tDict and sDict[s[left]] < tDict[s[left]]:
+                    have -= 1
                 left += 1
         return ans
-
+            
         
