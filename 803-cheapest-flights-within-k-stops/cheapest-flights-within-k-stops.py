08893b6288b75d40899266290124ab8859dc1a82
Shekhar <38083203+indianspeedster@users.noreply.github.com>
2024-10-15 13:45:40 -0400

    Time: 186 ms (19.18%) | Memory: 17.8 MB (62.68%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -1,22 +1,18 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        adjList = defaultdict(list)
+        distance = [float("inf") for i in range(n)]
+        distance[src] = 0
 
-        for start, end, charge in flights:
-            adjList[start].append((end, charge))
-        @cache
-        def dfs(node, stop):
-            if node == dst:
-                return 0
-            if stop == -1:
-                return float("inf")
-            maxi = float("inf")
-            for child in adjList[node]:
-                nextnode, charge = child
-                maxi = min(maxi, charge + dfs(nextnode, stop-1))
-            return maxi
-        
-        ans = dfs(src, k)
-        return -1 if ans == float("inf") else ans
+        for loop in range(k+1):
+            temp = distance.copy()
+
+            for start, end, charge in flights:
+                if distance[start] == float("inf"):
+                    continue
+                
+                if distance[start] + charge < temp[end]:
+                    temp[end] = distance[start] + charge
+            distance = temp
+        return -1 if distance[dst] == float("inf") else distance[dst]
 
         
