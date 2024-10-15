06dea4054bd4423bcba3a49ef3592a38ba2155cc
Shekhar <38083203+indianspeedster@users.noreply.github.com>
2024-10-14 21:12:39 -0400

    Time: 73 ms (49.31%) | Memory: 16.9 MB (7.31%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,20 +1,42 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        s1Count = defaultdict(int)
-        for s in s1:
-            s1Count[s] += 1
+        if len(s2) < len(s1):
+            return False
+        s1Dict = Counter(s1)
+        total = len(s1Dict.keys())
+        matches = 0
+        s2Dict = defaultdict(int)
         s1len = len(s1)
-        s2dict = defaultdict(int)
-        for s in s2[:s1len]:
-            s2dict[s] += 1
-        for i, s in enumerate(s2[:-s1len]):
-            if s2dict == s1Count:
+
+        for i in range(s1len):
+            char = s2[i]
+            if char in s1Dict and s2Dict[char] == s1Dict[char]:
+                matches -= 1
+            s2Dict[char] += 1
+            if char in s1Dict and s1Dict[char] == s2Dict[char]:
+                matches += 1
+
+        if matches == total:
+            return True
+        left = 0
+
+        for i, char in enumerate(s2[s1len:]):
+            if char in s1Dict and s2Dict[char] == s1Dict[char]:
+                matches -= 1
+            s2Dict[char] += 1
+            if char in s1Dict and s1Dict[char] == s2Dict[char]:
+                matches += 1
+            prev = s2[left]
+            if prev in s1Dict and s2Dict[prev] == s1Dict[prev]:
+                matches -= 1
+            s2Dict[prev] -= 1
+            if prev in s1Dict and s2Dict[prev] == s1Dict[prev]:
+                matches += 1
+            left += 1
+            if matches == total:
                 return True
-            s2dict[s2[i+s1len]] += 1
-            s2dict[s] -= 1
-            if s2dict[s] == 0:
-                del s2dict[s]
-        return s2dict == s1Count
+        return False
+
             
         
         
