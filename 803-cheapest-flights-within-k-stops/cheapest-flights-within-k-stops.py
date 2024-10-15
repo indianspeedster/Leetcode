d72c268f44ede31eb835837d3cfd7ef7e4ac59d6
Shekhar <38083203+indianspeedster@users.noreply.github.com>
2024-10-15 13:30:08 -0400

    Time: 190 ms (16.14%) | Memory: 20.3 MB (5.44%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -1,26 +1,22 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        adjList = defaultdict(list)
+
+        for start, end, charge in flights:
+            adjList[start].append((end, charge))
         @cache
-        def dfs(n, k):
-            if n == dst:
+        def dfs(node, stop):
+            if node == dst:
                 return 0
-            if k == -1 :
-                return float("inf")
-            if n not in dic:
+            if stop == -1:
                 return float("inf")
             maxi = float("inf")
-            for nodes in dic[n]:
-                #print(nodes)
-                maxi = min(maxi, nodes[1] + dfs(nodes[0], k-1))
+            for child in adjList[node]:
+                nextnode, charge = child
+                maxi = min(maxi, charge + dfs(nextnode, stop-1))
             return maxi
-        dic = {}
-        for source, dest, charge in flights:
-            if source not in dic:
-                dic[source] = [(dest, charge)]
-            else:
-                dic[source].append((dest, charge))
-        print(dic)
-        ans =  dfs(src, k) if src in dic else -1
         
-        return ans if ans != float("inf") else -1
+        ans = dfs(src, k)
+        return -1 if ans == float("inf") else ans
 
+        
