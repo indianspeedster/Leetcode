52763c7596b28c2e2b0eb0d72463d74a04e2618f
Shekhar <38083203+indianspeedster@users.noreply.github.com>
2024-10-29 22:09:31 -0400

    Time: 1852 ms (46.51%) | Memory: 16.9 MB (13.17%) - LeetSync

---
+++ 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minimumMountainRemovals(self, nums: List[int]) -> int:
+        N =len(nums)
+
+        lis = [1]*N
+
+        for i in range(N):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    lis[i] = max(lis[i], 1 + lis[j])
+
+        lds = [1]*N
+
+        for i in range(N-1, -1, -1):
+            for j in range(i+1, N):
+                if nums[i] > nums[j]:
+                    lds[i] = max(lds[i], 1+lds[j])
+        
+
+        res = N
+
+        for i in range(1,N-1):
+            if min(lis[i], lds[i]) > 1:
+                res = min(res, N- (lis[i]+lds[i]-1))
+        return res
+        
