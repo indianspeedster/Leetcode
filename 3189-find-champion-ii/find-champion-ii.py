5de15120d71ce45f9303ab497ba3a9bef6e98227
Shekhar <38083203+indianspeedster@users.noreply.github.com>
2024-11-25 22:54:14 -0500

    Time: 10 ms (54.97%) | Memory: 17.7 MB (80.31%) - LeetSync

---
+++ 3189-find-champion-ii/find-champion-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findChampion(self, n: int, edges: List[List[int]]) -> int:
+
+        inDegree = [0]*n
+
+        for node1, node2 in edges:
+            inDegree[node2] += 1
+
+        ans = -1
+
+        for i , val in enumerate(inDegree):
+            if val == 0 and ans == -1:
+                ans = i
+            if val == 0 and ans != i:
+                return -1
+        return ans
+        
