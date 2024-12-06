d4e94aa3b9af15aabffd1b00eee7fd74cdfa55b0
Shekhar <38083203+indianspeedster@users.noreply.github.com>
2024-12-06 18:02:00 -0500

    Time: 40 ms (83.00%) | Memory: 18.9 MB (18.29%) - LeetSync

--- 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.py
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.py
@@ -1,33 +1,22 @@
 class Solution:
-    def maxCount(self, banned: List[int], n: int, maxSum: int) -> int:
-       
-        banned.sort()
+    def maxCount(self, banned: list[int], n: int, maxSum: int) -> int:
+    
+        banned_set = set(banned)
+
         count = 0
 
+   
         for num in range(1, n + 1):
-            if self._custom_binary_search(banned, num):
+   
+            if num in banned_set:
                 continue
 
-            maxSum -= num
-            
-            if maxSum < 0:
-                break
 
+            if maxSum - num < 0:
+                return count
+
+         
+            maxSum -= num
             count += 1
 
         return count
-
-    def _custom_binary_search(self, arr: List[int], target: int) -> bool:
-        left, right = 0, len(arr) - 1
-
-        while left <= right:
-            mid = (left + right) // 2
-            if arr[mid] == target:
-                return True
-            if arr[mid] > target:
-                right = mid - 1
-            else:
-                left = mid + 1
-
-        return False
-        
