5a10f14899f7accbca717238674a59af9f4c570c
Shekhar <38083203+indianspeedster@users.noreply.github.com>
2024-12-09 18:15:42 -0500

    Time: 23 ms (99.12%) | Memory: 47.1 MB (32.17%) - LeetSync

--- 3427-special-array-ii/special-array-ii.py
+++ 3427-special-array-ii/special-array-ii.py
@@ -1,18 +1,12 @@
 class Solution:
     def isArraySpecial(self, nums: List[int], queries: List[List[int]]) -> List[bool]:
-
         N = len(nums)
-
         track = [1]*N
-
         for i in range(1, N):
             if nums[i]%2 != nums[i-1]%2:
                 track[i] = 1 + track[i-1]
-        ans = []
-
-        for start, end in queries:
+        ans = [False]*len(queries)
+        for index, (start, end) in enumerate(queries):
             if end - start +1 <= track[end]:
-                ans.append(True)
-            else:
-                ans.append(False)
+                ans[index] = True
         return ans
